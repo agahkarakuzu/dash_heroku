0a83330ad9d81fe57b50857a69f77869203556ed
Agah <agahkarakuzu@gmail.com>
2020-05-22 11:21:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
-
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 
-
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__)
 
 #######
 # YOUR APP HERE 
